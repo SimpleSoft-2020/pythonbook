ef77e7bd1b3848b4f63ac855324099fbfde70a7b
SimpleSoft-2020 <64841105+SimpleSoft-2020@users.noreply.github.com>
2020-07-05 14:09:10 +0800

    Add files via upload

---
+++ chap14/14-2/median_blur.py
@@ -0,0 +1,11 @@
+﻿import cv2
+#读取原始图像
+img = cv2.imread("blur_test.jpg")
+#使用中值滤波方式进行模糊
+blur_img =cv2.medianBlur(img,9) 
+#显示原始图像
+cv2.imshow("img",img)
+#显示模糊后的图像
+cv2.imshow("blur_img",blur_img)
+cv2.waitKey(0)
+
