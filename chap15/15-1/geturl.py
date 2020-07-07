e6d778c734d1f50d416d0be413b9338e2894ce01
SimpleSoft-2020 <64841105+SimpleSoft-2020@users.noreply.github.com>
2020-07-07 18:25:45 +0800

    Add files via upload

---
+++ chap15/15-1/geturl.py
@@ -0,0 +1,19 @@
+﻿import urllib.request
+import shutil
+import sys
+
+if(len(sys.argv) < 3):
+	print("请输入网址和要保存的文件名")
+	print("python geturl.py <url> <file name>")
+	sys.exit()
+
+url = sys.argv[1]
+file_name = sys.argv[2]
+#打开URL
+with urllib.request.urlopen(url) as response:
+	#打开文件
+	with open(file_name,'wb') as file:
+		#将response的内容写入到文件中
+		shutil.copyfileobj(response,file)
+
+
---
+++ chap15/15-1/post.py
@@ -0,0 +1,24 @@
+﻿import urllib.parse
+import urllib.request
+#post数据的url
+url = 'http://www.xxxx.com/submit'
+#要提交的数据
+values = {'name' : 'SimpleSoft',
+          'addr' : 'Beijing,China',
+          'age' : '28' ,
+          'phone':'010-11111111'}
+
+data = urllib.parse.urlencode(values)
+#取决于服务器端要求的格式
+#添加header信息,取决于服务器端是否需要
+headers = {
+    'Accept':'application/json, text/plain, */*',
+    'Connection':'keep-alive',
+    'Content-Length':'14', 
+    'Content-Type':'application/x-www-form-urlencoded',
+}
+data = data.encode('ascii') 
+
+req = urllib.request.Request(url, data,headers=headers)
+with urllib.request.urlopen(req) as response:
+   result = response.read()
